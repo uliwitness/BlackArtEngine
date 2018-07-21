918dff85c6874ef095902dae3e1632582012fae9
Uli Kusterer <ulivc@zathras.de>
2018-07-21 16:29:58 +0200

    Initial Commit

---
+++ BlackArtEngine/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BlackArtEngine
+//
+//  Created by Uli Kusterer on 21.07.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
